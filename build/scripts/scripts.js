bdc26ee06d6da118322cc7c9d508636f962f4cda
tenthirtyone <Spactigus@gmail.com>
2016-02-17 20:36:25 -0800

    Tearing out Bootstrap

--- build/scripts/scripts.js
+++ build/scripts/scripts.js
@@ -50,8 +50,29 @@
         'Content-Type': 'application/json'
     };     
     
+      var customPrimary = {
+        '50': '#46afed',
+        '100': '#2ea5eb',
+        '200': '#179be8',
+        '300': '#158cd1',
+        '400': '#127dba',
+        '500': '106da3',
+        '600': '#0e5d8c',
+        '700': '#0b4e75',
+        '800': '#093e5d',
+        '900': '#072f46',
+        'A100': '#5db9ef',
+        'A200': '#74c3f1',
+        'A400': '#8bcdf4',
+        'A700': '#051f2f'
+    };
+      $mdThemingProvider
+      .definePalette('customPrimary', 
+                      customPrimary);
+    
     $mdThemingProvider.theme('default')
-    .dark();
+      .primaryPalette('customPrimary');
+      
     
   }]);    
 }());
